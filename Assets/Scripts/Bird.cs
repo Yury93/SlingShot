6885185e197f5ad61e2d88beb7d9627b8315158f
Yury93 <72685333+Yury93@users.noreply.github.com>
2021-12-17 01:41:56 +0300

    ComplitedSlingShoot

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -9,65 +9,39 @@ public class Bird : MonoBehaviour
     [SerializeField] private float maxSpeed;
     [SerializeField]private float speed = 3.5f;
     private GameObject factoryBird;
-    /// <summary>
-    /// Рассчёт траектории
-    /// </summary>
-    [SerializeField]private TrailRenderer trailRender;
-    private GameObject trailGo;
-    [SerializeField] private Rigidbody rbTrail;
     private void Start()
     {
-
         gameObject.GetComponent<Bird>().enabled = true;
         ribody = GetComponent<Rigidbody>();
         ribody.isKinematic = true;
-
-        rbTrail = GetComponentInChildren<Rigidbody>();
-        rbTrail.isKinematic = true;
-        trailRender = GetComponentInChildren<TrailRenderer>();
-        trailGo = GetComponentInChildren<TrailRenderer>().gameObject;
-
-
         factoryBird = gameObject.GetComponentInParent<BirdFactory>().gameObject;
     }
     void Update()
     {
-        if (rbTrail == null)
+        if (Application.platform == RuntimePlatform.Android)
         {
-            Start();
-
-        }
-            if (Application.platform == RuntimePlatform.Android)
-            {
             if (Input.GetMouseButton(0))
             {
                 if (speed <= maxSpeed)
                 {
                     speed += Time.deltaTime;
                 }
-
-                rbTrail.isKinematic = false;
-                Vector3 dir = rbTrail.transform.position - gameObject.transform.position;
-                rbTrail.AddForce(dir * speed, ForceMode.Impulse);
             }
-            if (Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButton(0))
             {
                 ribody.isKinematic = false;
                 Vector3 dir = factoryBird.transform.position - gameObject.transform.position;
                 ribody.AddForce(dir * speed, ForceMode.Impulse);
             }
-            }
-            else
-            {
+        }
+        else
+        {
             if (Input.GetKey(KeyCode.Space))
             {
                 if (speed <= maxSpeed)
                 {
                     speed += Time.deltaTime;
                 }
-                rbTrail.isKinematic = false;
-                Vector3 dir = rbTrail.transform.position - gameObject.transform.position;
-                rbTrail.AddForce(dir * speed, ForceMode.Impulse);
             }
             if (Input.GetKeyUp(KeyCode.Space))
             {
